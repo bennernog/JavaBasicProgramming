ca2cad982b75b9469faae0aa829344ea73aa68ee
Ubuntu <bnymobile@penguin.lxd>
2021-04-12 09:29:43 +0200

    ex 136 added

--- src/basic/part1/ex131140/Exercise136.java
+++ src/basic/part1/ex131140/Exercise136.java
@@ -1,5 +1,7 @@
 package basic.part1.ex131140;
 
+import java.util.stream.LongStream;
+
 /*
     136. Write a Java program to find possible unique paths from top-left corner to bottom-right corner of a given
     grid (m x n).
@@ -7,4 +9,16 @@ package basic.part1.ex131140;
  */
 public class Exercise136 {
 
+    public static void main(String[] args) {
+        System.out.println(findNumberOfPathsInGrid(2, 3));
+    }
+
+    public static long findNumberOfPathsInGrid(int columns, int rows) {
+        return factorial(columns + rows - 2) / (factorial(columns - 1) * factorial(rows - 1));
+    }
+
+    public static long factorial(int n) {
+        return LongStream.rangeClosed(1, n)
+                .reduce(1, (long x, long y) -> x * y);
+    }
 }
